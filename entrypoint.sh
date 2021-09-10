fb37902f0e7bc170d8d4fb67fadcb9683f802fe0
Michael Gearhardt <michael.gearhardt@gmail.com>
2021-09-10 12:25:06 -0600

    Try this

--- entrypoint.sh
+++ entrypoint.sh
@@ -76,11 +76,11 @@ tag_commit=$(git rev-list -n 1 $tag)
 # get current commit hash
 commit=$(git rev-parse HEAD)
 
-if [ "$tag_commit" == "$commit" ]; then
-    echo "No new commits since previous tag. Skipping..."
-    echo ::set-output name=tag::$tag
-    exit 0
-fi
+# if [ "$tag_commit" == "$commit" ]; then
+#     echo "No new commits since previous tag. Skipping..."
+#     echo ::set-output name=tag::$tag
+#     exit 0
+# fi
 
 # echo log if verbose is wanted
 if $verbose
